8e54bfe7d83269bcf2060fd5d58785489b360c8a
hamza <hamza.ouggadi@gmail.com>
2023-02-07 17:24:11 +0100

    //

--- faces_train.py
+++ faces_train.py
@@ -1,1 +1,11 @@
 import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(os.path.dirname(path))
+            print(label, path)
