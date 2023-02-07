190441c6ccb2dc46c5384440072cf39ffb96c4f6
Hamza Ouggadi <45943796+OddToast@users.noreply.github.com>
2023-02-07 19:36:56 +0100

    //

--- faces_train.py
+++ faces_train.py
@@ -1,4 +1,6 @@
 import os
+import numpy as np
+from PIL import Image
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
@@ -9,3 +11,6 @@ for root, dirs, files in os.walk(image_dir):
             path = os.path.join(root, file)
             label = os.path.basename(os.path.dirname(path))
             print(label, path)
+            pil_image = Image.open(path).convert("L") # grayscale
+            image_array = np.array(pil_image, "uint8")
+            print(image_array)
