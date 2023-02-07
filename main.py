11de0e1b0279872119fe4b87c860e177063cde7f
hamza <hamza.ouggadi@gmail.com>
2023-02-07 16:19:20 +0100

    Facial detection added

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ while(True):
     # Capturing frame by frame
     ret, frame = cap.read()
 
+    grayedFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(grayedFrame, scaleFactor=1.5, minNeighbors=5)
+
+    for (x, y, w, h) in faces:
+        print(x, y, w, h)
+
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(20) & 0xFF == ord('q'):
